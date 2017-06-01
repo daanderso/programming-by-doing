31bbafc93ffa19227462792908474ac527027d6e
Derek <daanderso@gmail.com>
2017-05-31 22:51:38 -0400

    arraylist0 exercise

---
+++ arraylist/ArrayListsZero.java
@@ -0,0 +1,38 @@
+package arraylist;
+import java.util.ArrayList;
+
+public class ArrayListsZero {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		
+		//using add to add elements to an arraylist
+		numbers.add(-113);
+		numbers.add(-113);
+		numbers.add(-113);
+		numbers.add(-113);
+		numbers.add(-113);
+		numbers.add(-113);
+		numbers.add(-113);
+		numbers.add(-113);
+		numbers.add(-113);
+		numbers.add(-113);
+		
+		System.out.println(numbers);
+		
+		//use get to reference the index of an element in arraylist
+		System.out.println("Slot 0 contains a "+ numbers.get(0));
+		System.out.println("Slot 1 contains a "+ numbers.get(1));
+		System.out.println("Slot 2 contains a "+ numbers.get(2));
+		System.out.println("Slot 3 contains a "+ numbers.get(3));
+		System.out.println("Slot 4 contains a "+ numbers.get(4));
+		System.out.println("Slot 5 contains a "+ numbers.get(5));
+		System.out.println("Slot 6 contains a "+ numbers.get(6));
+		System.out.println("Slot 7 contains a "+ numbers.get(7));
+		System.out.println("Slot 8 contains a "+ numbers.get(8));
+		System.out.println("Slot 9 contains a "+ numbers.get(9));
+		
+		
+	}
+
+}
