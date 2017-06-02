cad1bc3c57af81557f8a08cc783d3ffbd2b95d59
Derek <daanderso@gmail.com>
2017-06-01 22:20:02 -0400

    For Loop exercises

---
+++ ForLoops/CountingByHalves.java
@@ -0,0 +1,16 @@
+package ForLoops;
+
+public class CountingByHalves {
+
+	public static void main(String[] args) {
+		double counter;
+		System.out.println("x");
+		System.out.println("---");
+		
+		for (counter = -10; counter <= 10; counter = counter + 0.5) {	
+			System.out.println(counter);
+		}
+
+	}
+
+}
