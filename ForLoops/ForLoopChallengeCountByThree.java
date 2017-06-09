c12f66d4fbcdb589dcd1784b43a3850ba3505ad2
Derek <daanderso@gmail.com>
2017-06-08 21:39:44 -0400

    For Loop Count by 3

---
+++ ForLoops/ForLoopChallengeCountByThree.java
@@ -0,0 +1,13 @@
+package ForLoops;
+
+public class ForLoopChallengeCountByThree {
+
+	public static void main(String[] args) {
+
+		for(int j =15; j <= 30; j=j+3){
+			System.out.println(j);
+			
+		}
+	}
+
+}
