988836b00cc369fda470be204f90dbafb74911c3
Derek <daanderso@gmail.com>
2017-07-25 10:59:46 -0400

    WeekdayName exercise

---
+++ Functions/WeekdayName.java
@@ -0,0 +1,29 @@
+package Functions;
+
+public class WeekdayName {
+	
+	public static String weekday_name(int weekday) {
+		String result = "";
+
+		if (weekday == 1) {
+			result = "Sunday";
+		} else if (weekday == 2) {
+			result = "Monday";
+		} else if (weekday == 3) {
+			result = "Tuesday";
+		} else if (weekday == 4) {
+			result = "Wednesday";
+		} else if (weekday == 5) {
+			result = "Thursday";
+		}else if ( weekday == 6 )
+		{
+			result = "Friday";
+		}
+		else if ( weekday == 7 )
+		{
+			result = "Saturday";
+		}
+
+		return result;
+	}
+}
