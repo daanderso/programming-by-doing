a40dc63f266c6d0a0fe889384759876dac85e41b
Derek <daanderso@gmail.com>
2017-07-27 16:30:24 -0400

    Getting individual digits

---
+++ nested_loops/GettingIndividualDigits.java
@@ -0,0 +1,17 @@
+package nested_loops;
+
+public class GettingIndividualDigits {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 10; i++) {
+
+			for (int j = 0; j <= 9; j++) {
+				int result = i + j;
+				System.out.println((10*i+j)+", "+ + i + " + " + j + " = " + result);
+			} // for 1
+
+		} // for 2
+
+	}
+}
